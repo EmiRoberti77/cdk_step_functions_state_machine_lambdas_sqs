97f5042d051db80267296fb3b2dffc56ab5acf9a
EmiRoberti77 <emi@datamotion.ai>
2024-08-17 18:54:11 +0100

    Initial commit

---
+++ test/state_machine_sqs_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StateMachineSqsLambda from '../lib/state_machine_sqs_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/state_machine_sqs_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StateMachineSqsLambda.StateMachineSqsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
